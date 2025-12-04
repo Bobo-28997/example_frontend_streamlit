9f0a4d4b325edca46aac68a4ae4cbb46c988b4a7
Bowen Bao <bbao28997@gmail.com>
2025-12-04 14:41:59 +0800

    Update __init__.py

--- my_component/__init__.py
+++ my_component/__init__.py
@@ -1,10 +1,13 @@
 import os
 import streamlit.components.v1 as components
 
+# 获取组件目录的绝对路径
+_component_dir = os.path.dirname(os.path.abspath(__file__))
+
 # 声明组件
 _component_func = components.declare_component(
     "simple_component",
-    path=os.path.dirname(os.path.abspath(__file__))
+    path=_component_dir
 )
 
 def simple_component(name="World", key=None):
