4e83eeae0e82394a2b3fdc5c0725516550ad1945
Bowen Bao <bbao28997@gmail.com>
2025-12-04 14:23:43 +0800

    Update __init__.py

--- my_component/__init__.py
+++ my_component/__init__.py
@@ -1,64 +1,13 @@
-import streamlit as st
+import os
+import streamlit.components.v1 as components
 
-out = st.components.v2.component(
-    "streamlit-custom-component.my_component",
-    js="index-*.js",
-    html="""
-        <div class="component-root">
-            <span>
-                <h1></h1>
-                <button>Click me!</button>
-            </span>
-        </div>
-    """,
+# 声明组件
+_component_func = components.declare_component(
+    "simple_component",
+    path=os.path.dirname(os.path.abspath(__file__))
 )
 
-
-def on_num_clicks_change():
-    """Callback function for when the number of clicks changes in the frontend."""
-    pass
-
-
-# Create a wrapper function for the component.
-#
-# This is an optional best practice. We could simply expose the component
-# function returned by `st.components.v2.component` and call it done.
-#
-# The wrapper allows us to customize our component's API: we can pre-process its
-# input args, post-process its output value, and add a docstring for users.
-def my_component(name, key=None):
-    """Create a new instance of "my_component".
-
-    Parameters
-    ----------
-    name: str
-        The name of the thing we're saying hello to. The component will display
-        the text "Hello, {name}!"
-    key: str or None
-        An optional key that uniquely identifies this component.
-
-    Returns
-    -------
-    int
-        The number of times the component's "Click Me" button has been clicked.
-        (This is the value passed to `Streamlit.setComponentValue` on the
-        frontend.)
-
-    """
-    # Call through to our private component function. Arguments we pass here
-    # will be sent to the frontend, where they'll be available in an "args"
-    # dictionary.
-    #
-    # "default" is a special argument that specifies the initial return
-    # value of the component before the user has interacted with it.
-    component_value = out(
-        name=name,
-        key=key,
-        default={"num_clicks": 0},
-        data={"name": name},
-        on_num_clicks_change=on_num_clicks_change,
-    )
-
-    # We could modify the value returned from the component if we wanted.
-    # There's no need to do this in our simple example - but it's an option.
+def simple_component(name="World", key=None):
+    """创建一个简单的Hello World组件"""
+    component_value = _component_func(name=name, key=key, default=None)
     return component_value
